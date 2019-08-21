90d88d6fddaf5d7ca1989578f792d90dab37f39b
プル ザキム <ckg17999@st.ckg.ac.jp>
2019-08-21 17:16:42 +0900

    okasii

--- album/js/index.js
+++ album/js/index.js
@@ -1,7 +1,26 @@
 window.addEventListener('DOMContentLoaded',
   function() {
     // ページ本体が読み込まれたタイミングで実行するコード
-    let result = document.getElementById('result');
-    result.textContent = 'Hello, JavaScript!!';
-  }, false
+    let mysubject =document.getElementById('subject');
+    let mymemo =document.getElementById('memo');
+    let mysave =document.getElementById('save');
+    let mycancel =document.getElementById('cancel');
+    mycancel.addEventListener(
+      'click',
+      function(){
+        location.href='index.html';
+      },false
+    );
+  
+
+    mysave.addEventListener(
+      'click',
+      function(){
+
+        alert('save');
+      },false
+    );
+    
+  },false
 );
+
