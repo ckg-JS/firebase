d48654daeb31c216c876bf6b910738b8a1296bc3
17546 <17546>
2019-09-03 12:33:48 +0900

    ok

---
+++ album/js/input.js
@@ -0,0 +1,14 @@
+window.addEventListener(
+'DOMContentLoaded',
+function(){
+let mycancel = document.getElementById('cancel');
+mycancel.addEventListener(
+    'click',
+    function(){
+        location.href ="index.html"
+    },
+    false
+);
+},
+false
+);
